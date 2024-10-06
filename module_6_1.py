3f46feb119c84c24c42f9a28304f8299adfe1aa5
Андрей <an-shafra2013@ya.ru>
2024-10-06 12:47:25 +0800

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,51 @@
+class Animal:  # Родительский класс
+
+    alive = True
+    fed = False
+
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        if isinstance(food, Plant):
+            if food.edible:
+                print(f'{self.name} съел {food.name}')
+                self.fed = True
+            else:
+                print(f'{self.name} не стал есть {food.name}')
+                self.alive = False
+
+
+class Plant:  #Родительский класс
+    edible = False
+    def __init__(self, name):
+        self.name = name
+
+class Mammal(Animal):  # Класс наследник
+    pass
+
+class Predator(Animal):  # Класс наследник
+    pass
+
+class Flower(Plant):  # Класс наследник
+    pass
+
+class Fruit(Plant):   # Класс наследник, который через наследование переопределяет атрибут 'edible' в классе Plant.
+    def __init__(self,name):
+        super().__init__(name)
+        self.edible = True
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
